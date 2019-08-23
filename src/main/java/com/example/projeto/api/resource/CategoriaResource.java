abe4b207891fb43de60b7961bbcd7b372f4228ed
Gabriella Barreto <gabriellabarretogomes@gmail.com>
2019-08-23 10:42:11 -0300

    Adicionando endpoints e validação de atributos desconhecidos

--- src/main/java/com/example/projeto/api/resource/CategoriaResource.java
+++ src/main/java/com/example/projeto/api/resource/CategoriaResource.java
@@ -1,11 +1,23 @@
 package com.example.projeto.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.h2.command.ddl.CreateAggregate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.projeto.api.model.Categoria;
 import com.example.projeto.api.repository.CategoriaRepository;
@@ -21,4 +33,23 @@ public class CategoriaResource {
 	public List<Categoria> listarCategorias() {
 		return categoriaRepository.findAll();
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criarCategoria(@RequestBody Categoria categoria, HttpServletResponse response){
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+		
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> listarPorCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo);
+	}
+	
+	
 }
