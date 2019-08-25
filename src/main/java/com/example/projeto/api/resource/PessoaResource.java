127d54bad98649ef184a2f70742fbe9be9bd83d6
Gabriella Barreto <gabriellabarretogomes@gmail.com>
2019-08-25 16:57:13 -0300

    Corrigindo um problema no pom.xml

--- src/main/java/com/example/projeto/api/resource/PessoaResource.java
+++ src/main/java/com/example/projeto/api/resource/PessoaResource.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +51,11 @@ public class PessoaResource {
 	@GetMapping("/{codigo}")
 	public Optional<Pessoa> listarPessoaPorCodigo(@PathVariable Long codigo) {
 		return pessoaRepository.findById(codigo);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
 	}
 }
