35a642a5b647e12a8eea62a7dfd7a2bfeff42c5b
Gabriella Barreto <gabriellabarretogomes@gmail.com>
2019-08-25 13:45:36 -0300

    Fazendo ajuste na classe ExceptionHandler

--- src/main/java/com/example/projeto/api/exceptionhandler/CategoriaExceptionHandler.java
+++ src/main/java/com/example/projeto/api/exceptionhandler/CategoriaExceptionHandler.java
@@ -14,10 +14,11 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
+@ControllerAdvice
 public class CategoriaExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Autowired
@@ -31,7 +32,6 @@ public class CategoriaExceptionHandler extends ResponseEntityExceptionHandler {
 		String mensagemDesenvolvedor = ex.getCause().toString();
 
 		List<Erro> erro = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
-
 		return handleExceptionInternal(ex, erro, headers, HttpStatus.BAD_REQUEST, request);
 	}
 
