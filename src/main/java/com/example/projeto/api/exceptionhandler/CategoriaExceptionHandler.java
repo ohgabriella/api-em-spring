e5a4544c24b4871fe6534c581c13a9bc04bde428
Gabriella Barreto <gabriellabarretogomes@gmail.com>
2019-08-25 21:40:25 -0300

    Adição do verbo Delete

--- src/main/java/com/example/projeto/api/exceptionhandler/CategoriaExceptionHandler.java
+++ src/main/java/com/example/projeto/api/exceptionhandler/CategoriaExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -53,6 +55,20 @@ public class CategoriaExceptionHandler extends ResponseEntityExceptionHandler {
 		}
 
 		return erros;
+	}
+
+	@ExceptionHandler({ EmptyResultDataAccessException.class })
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
+			WebRequest request) {
+
+		String mensagemUsuario = messageSource.getMessage("mensagem.recurso-nao-encontrado", null,
+				LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+
+		List<Erro> erro = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+
+		return handleExceptionInternal(ex, erro, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+
 	}
 
 	public class Erro {
